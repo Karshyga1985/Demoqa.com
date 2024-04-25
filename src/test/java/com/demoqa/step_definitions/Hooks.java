9d188fb8395963bb2a6e62801290640a27ab6fe9
perizat5 <aperizat45@gmail.com>
2024-04-25 15:55:50 -0500

    added new code lines

--- src/test/java/com/demoqa/step_definitions/Hooks.java
+++ src/test/java/com/demoqa/step_definitions/Hooks.java
@@ -1,4 +1,36 @@
 package com.demoqa.step_definitions;
 
+import com.demoqa.utilities.ConfigurationReader;
+import com.demoqa.utilities.Driver;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import io.cucumber.java.Scenario;
+
+import java.time.Duration;
+
 public class Hooks {
+    @Before(order = 1)
+    public void setupMethod(){
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+
+    @After
+    public void teardownMethod(Scenario scenario){
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+
+        Driver.closeDriver();
+    }
+    // db connection
+
+
+
+    //@BeforeStep
+
+
 }
